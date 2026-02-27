80f028ac40beb2a921a07832e8e8a96baf085b73
Luyao Shi <luyaoshi6@gmail.com>
2026-02-26 23:43:14 -0800

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by Shi, Luyao on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
